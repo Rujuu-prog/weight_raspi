5611123014271277a90829c96b5f79a2a30c7c15
Rujuu <n.ujiie2118@gmail.com>
2021-12-30 04:15:49 +0900

    add test print2

--- weight_calc.py
+++ weight_calc.py
@@ -45,20 +45,25 @@ def main():
     hx = setting(refUnit)
 
     while True:
-        val = measurement(hx)
-        weight = math.modf(val)
-        print(weight)
-        if count < 10 and before_weight == weight:
-            count += 1
-            before_weight = weight[0]
+        try:
+            val = measurement(hx)
+            weight = math.modf(val)
+            print(weight)
             print(count)
-        elif count == 10 and before_weight == weight:
-            val_list.append(val)
-            if len(val_list) == 30:
-                break
-        else:
-            print("例外です。")
-            continue
+            if count < 10 and before_weight == weight:
+                count += 1
+                before_weight = weight[0]
+                
+            elif count == 10 and before_weight == weight:
+                val_list.append(val)
+                if len(val_list) == 30:
+                    break
+            else:
+                print("例外です。")
+                continue
+        except(KeyboardInterrupt, SystemExit):
+            # panel.display_clear()
+            cleanAndExit()
     avg = sum(val_list) / len(val_list)
     refUnit = avg // def_weight
     print("キャリブレーション値は、"+str(refUnit)+"です。")

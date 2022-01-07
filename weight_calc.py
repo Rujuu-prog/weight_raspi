3768c56a3385cc5f8ee2fa5689eb968e21a3daaa
Rujuu <n.ujiie2118@gmail.com>
2022-01-07 15:42:19 +0900

    add output file func

--- weight_calc.py
+++ weight_calc.py
@@ -2,6 +2,8 @@ import time
 import sys
 import os
 import math
+import datetime
+import pandas as pd
 from decimal import Decimal, ROUND_HALF_UP
 import RPi.GPIO as GPIO
 from hx711py.hx711 import HX711
@@ -9,7 +11,10 @@ from hx711py.hx711 import HX711
 
 PIN_DAT = 5
 PIN_CLK = 6
-
+dt_now = datetime.datetime.now()
+dt_now_year = str(dt_now.year)
+dt_now_month = str(dt_now.month)
+dt_now_day = str(dt_now.day)
 
 # Exit関数()
 def cleanAndExit() -> None:
@@ -39,6 +44,7 @@ def main() -> None:
     # 初期値
     refUnit = 1
     count = 0
+    id = 1
     before_weight = 0
     val_list = []
 
@@ -47,6 +53,14 @@ def main() -> None:
     container_weight = int(input("2.Enter the weight of the container...."))
     container_width = int(input("3.Enter the width of the container in mm...."))
     container_vertical = int(input("4.Enter the vertical of the container in mm...."))
+    while True:
+        file_name = int(input("5.Enter a name for the output file without extension...."))
+        file_path = "./output/"+dt_now_year+"_"+dt_now_month+"_"+dt_now_day+"/"+file_name+".csv"
+        if os.path.isfile(file_path):
+            print("It already exists file name.")
+            continue
+        else:
+            break
 
     # キャリブレーション開始
     hx = setting(refUnit)
@@ -79,20 +93,28 @@ def main() -> None:
     refUnit = avg // cal_weight
     print("Calibration value is "+str(refUnit)+".")
     input("Please input enter to start measurement...")
+    
+    # 出力ファイル作成
+    df_def = pd.read_csv("./default_file/default.csv", dtype=str, encoding='SHIFT-JIS', engine='python')
+    df_def.to_csv(file_path, encoding='SHIFT-JIS', index=False)
 
     # 計測開始
     print("-----Start measurement-----")
     hx = setting(refUnit)
     start_time = time.perf_counter()
+    # df = pd.read_csv(file_path, dtype=str, encoding='SHIFT-JIS', engine='python')
     while True:
         try:
             val = measurement(hx, 1)
-            print("#---------------------")
+            print("#"+str(id)+"---------------------")
             water_weight = val - container_weight
             print("■"+str(water_weight)+"g")
             elapsed_time = Decimal(str(time.perf_counter() - start_time)).quantize(Decimal('0'), rounding=ROUND_HALF_UP)
             precipitation = (water_weight * 1000) / (container_width * container_vertical) * (3600 / int(elapsed_time))
             print("●"+str(precipitation)+"mm/h")
+            df = pd.DataFrame([[id, elapsed_time, water_weight, precipitation]])
+            df.to_csv(file_path, mode='a', encoding='SHIFT-JIS', index=False, header=False)
+            id += 1
         except(KeyboardInterrupt, SystemExit):
             cleanAndExit()
 
